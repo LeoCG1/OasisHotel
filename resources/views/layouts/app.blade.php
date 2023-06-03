296d64de2cd337010fb7086f0c5d0e9936719f5d
LeoCG1 <93930586+LeoCG1@users.noreply.github.com>
2023-06-03 13:38:45 +0200

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,8 +6,8 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Hotel Oasis - @yield('titulo')</title>
-  <!-- <link rel="stylesheet" href="/css/app-57681ef3.css"> -->
-  @vite(['resources/css/app.scss'])
+  <link rel="stylesheet" href="/css/app-57681ef3.css">
+  
   <style>
   @import url('https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap');
 </style>
